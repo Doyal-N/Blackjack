707be792285061fca5698545fe3aa325b90f77ef
Doyal-N <coin20@protonmail.com>
2020-07-26 19:23:36 +0500

    check class DeckofCards

--- deck.rb
+++ deck.rb
@@ -1,8 +1,23 @@
+require_relative 'card'
+
 class DeckOfCards
-  SUITS = [♥, ♦, ♣, ♠]
+  
+  attr_reader :deck
+
+  def initialize
+    @deck = get_cards
+  end
+
+  SUITS = ['♥', '♦', '♣', '♠']
   SIGNS = %w(2 3 4 5 6 7 8 9 10 J Q K A)
   
-
-
-
+  def get_cards 
+    deck = []
+    SUITS.each do |suit|
+      SIGNS.each do |sign|
+        deck << Card.new(suit, sign)  
+      end 
+    end
+    deck.shuffle  
+  end
 end
