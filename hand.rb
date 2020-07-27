77cb959fdcf5b71864ab1dee9646c54249f3d23a
Doyal-N <coin20@protonmail.com>
2020-07-27 15:56:40 +0500

    class hand

---
+++ hand.rb
@@ -0,0 +1,17 @@
+class Hand
+  def initialize
+    shoe = Shoe.new
+    @cards = shoe.cards    
+  end
+
+  def deal_cards(count = 2)
+    @cards.sample(count).each { |card| @cards.delete card }
+  end
+
+  def deal_card
+    deal_cards(1)
+  end  
+
+
+
+end
