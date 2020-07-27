d9d3fa35ed05d3b45b664916c07d428b7c8414c9
Doyal-N <coin20@protonmail.com>
2020-07-27 21:45:18 +0500

    fix diler

--- diler.rb
+++ diler.rb
@@ -1,5 +1,17 @@
 class Diler < User
+  
+  PASS = 17
+
   def initialize(name = 'Croupier')
     super
   end  
+
+  def can_pass?(score)
+    score >= PASS
+  end  
+  
+  def take_card?(score)
+    score < PASS
+  end
+
 end
